d5d04ba75bd2b9cb38ffe7c7264ff44c0463387f
Marco Werner <marco.werner@dpdhl.com>
2020-06-26 16:10:41 +0200

    add version app

---
+++ version/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+app = express();
+
+var response;
+
+app.get('/', function(req, res) {
+    res.send('This is version 1 of the app.\n');
+});
+
+app.listen(8080, function() {
+    console.log('Server listening on port 8080...');
+});
